ad55146c183f7ec9f9109d35d0a6de39df3cd980
bannsec <whootandahalf@gmail.com>
2019-04-04 13:05:46 -0400

    Adding layer iterator helper

---
+++ scapyshark/helpers.py
@@ -0,0 +1,24 @@
+
+def iter_layers_by_type(packet, target_type, allow_subclass=False):
+
+    # Scapy doesn't handle iterators very well...
+    i = 0
+
+    if target_type not in packet:
+        raise StopIteration
+
+    while True:
+
+        try:
+            curr = packet[i]
+        except IndexError:
+            raise StopIteration
+
+        if allow_subclass and isinstance(curr, target_type):
+            yield curr
+
+        elif not allow_subclass and type(curr) == target_type:
+            yield curr
+
+        i += 1
+
